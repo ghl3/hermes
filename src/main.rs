046be892ddba302a6f7784146f844ae53f8861ed
George Lewis <ghl227@gmail.com>
2015-12-15 22:30:55 -0500

    Start TCP listener on supplied port

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@ extern crate docopt;
 
 use docopt::Docopt;
 
-use std::net::{TcpListener, TcpStream};
 use std::thread;
 
+use std::net::{TcpListener, TcpStream};
+use std::net::SocketAddr;
 
+use std::io::Read;
+use std::str::FromStr;
 
 const USAGE: &'static str = "
 Naval Fate.
@@ -25,14 +28,34 @@ Options:
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    flag_port: String
+    flag_port: i32
 }
 
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = Vec::<u8>::new();
+    let data = stream.read_to_end(&mut buffer).unwrap();
+    println!("{}", data);
+}
 
 fn main() {
+
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
     println!("Listening on port: {:?}", args.flag_port);
+
+    let addr: SocketAddr = SocketAddr::from_str(&format!("127.0.0.1:{}", args.flag_port)).unwrap();
+    let listener = TcpListener::bind(addr).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => { println!("ERROR: {:}", e); }
+        }
+    }
 }
