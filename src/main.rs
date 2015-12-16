f7bf2b41c57a5594b4b0da050da92bb9327f35da
George Lewis <ghl227@gmail.com>
2015-12-15 21:49:46 -0500

    Add argument for port to listen on

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+
+
+const USAGE: &'static str = "
+Naval Fate.
+
+Usage:
+  rust_redis [--port=<p>]
+  naval_fate.py (-h | --help)
+  naval_fate.py --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --port=<p>    Port to listen on [default: 5001]
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    flag_port: String
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    println!("Listening on port: {:?}", args.flag_port);
 }
