1dcd26c06ed0dcb2e38bfe6b7965ccfa6e7361ab
George Lewis <ghl227@gmail.com>
2015-12-16 21:24:07 -0500

    Implement basic HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 
 extern crate rustc_serialize;
 extern crate docopt;
+extern crate tiny_http;
 
 use docopt::Docopt;
 
@@ -10,7 +11,13 @@ use std::net::{TcpListener, TcpStream};
 use std::net::SocketAddr;
 
 use std::io::Read;
+use std::io::Cursor;
+
 use std::str::FromStr;
+
+
+use tiny_http::{Server, Request, Response, StatusCode, Method, Header};
+
 
 
 const USAGE: &'static str = "
@@ -39,6 +46,69 @@ fn handle_client(mut stream: TcpStream) {
     println!("{}", String::from_utf8(buffer).unwrap());
 }
 
+
+fn handle_request(mut request: Request) {
+    //println!("Headers: {}", request.headers());
+    let mut content = String::new();
+    request.as_reader().read_to_string(&mut content).unwrap();
+    println!("Body: {}", content);
+
+    let response = match request.method() {
+        &Method::Get => handle_get(&request),
+        &Method::Post => handle_post(&request),
+        &Method::Put => handle_put(&request),
+        &Method::Delete => handle_delete(&request),
+        _ => handle_nonstandard_request(&request)
+    };
+
+    request.respond(response);
+}
+
+
+//static headers: Vec<tiny_http::Header> = Vec::new();
+//static header: Header = tiny_http::Header::from_bytes(&b"Content-Type", &b"text/plain").unwrap();
+
+
+
+fn http_response<S>(status: StatusCode, data: S) -> Response<Cursor<Vec<u8>>> where S: Into<String> {
+
+        let data = data.into();
+        let data_len = data.len();
+
+        Response::new(
+            status,
+            vec![
+                Header::from_bytes(&b"Content-Type"[..], &b"text/plain; charset=UTF-8"[..]).unwrap()
+                    ],
+            Cursor::new(data.into_bytes()),
+            Some(data_len),
+            None,
+            )
+}
+
+fn handle_get(request: &Request) -> Response<Cursor<Vec<u8>>> {
+    println!("GET");
+    http_response(StatusCode(200), "COOL")
+    //Response::new(StatusCode(200), headers, "COOL", Option::None, Option::None)
+}
+fn handle_post(request: &Request) -> Response<Cursor<Vec<u8>>> {
+    println!("POST");
+    http_response(StatusCode(200), "COOL")
+}
+fn handle_put(request: &Request) -> Response<Cursor<Vec<u8>>> {
+    println!("PUT");
+    http_response(StatusCode(200), "COOL")
+}
+fn handle_delete(request: &Request) -> Response<Cursor<Vec<u8>>> {
+    println!("DELETE");
+    http_response(StatusCode(200), "COOL")
+}
+fn handle_nonstandard_request(request: &Request) -> Response<Cursor<Vec<u8>>> {
+    println!("WTF!!!");
+    http_response(StatusCode(200), "COOL")
+}
+
+
 fn main() {
 
     let args: Args = Docopt::new(USAGE)
@@ -48,6 +118,22 @@ fn main() {
     println!("Listening on port: {:?}", args.flag_port);
 
     let addr: SocketAddr = SocketAddr::from_str(&format!("127.0.0.1:{}", args.flag_port)).unwrap();
+
+    let server = Server::http(addr).unwrap();
+
+
+    loop {
+        // blocks until the next request is received
+        let request = match server.recv() {
+            Ok(rq) => handle_request(rq),
+            Err(e) => { println!("error: {}", e); break }
+        };
+
+        // do something with the request
+        // ...
+        }
+
+/*
     let listener = TcpListener::bind(addr).unwrap();
 
     for stream in listener.incoming() {
@@ -62,4 +148,5 @@ fn main() {
             }
         }
     }
+*/
 }
